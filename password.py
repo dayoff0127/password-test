27c463c245cb6729dda51c95d60726ccbedae96b
Boyu Wu <rhetoric28569@gmail.com>
2020-09-04 12:36:03 +0800

    secong

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 password = 'a123456'
-n = 2
+n = 3
 while True :
 	a = input('請輸入密碼:')
 	if a == password :
 		print('登入成功')
 		break
 	else :
-		print('密碼錯誤! 還有', n, '次機會')
 		n = n - 1
-		if n == -1 :
+		print('密碼錯誤! 還有', n, '次機會')
+		if n == 0 :
 			break
